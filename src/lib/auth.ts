541e1f51d4103474639e0ed5bf7f40de0144d425
Snu Seam <siamahme766@gmail.com>
2026-06-10 12:59:06 +0600

    otp

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,34 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { twoFactor } from "better-auth/plugins"
+
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
+
+    appName: "Lab log",
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
-    emailAndPassword:{
-        enabled : true
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+            redirectURI: `${process.env.FRONTEND_URL}/api/auth/callback/github`
+        }
     },
-     trustedOrigins: [process.env.FRONTEND_URL!.trim()]
+    emailAndPassword: {
+        enabled: true
+    },
+    trustedOrigins: [process.env.FRONTEND_URL!],
+    plugins: [
+        twoFactor({
+          	otpOptions: {
+				async sendOTP({ user, otp }, ctx) {
+                    console.log(user,otp);
+				},
+			},
+        })
+    ]
 });
